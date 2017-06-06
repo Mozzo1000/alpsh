2a9f2fe786018eb6e2b8756ffac4ba36c7f208f5
Mozzo <mozzo242@gmail.com>
2017-06-06 09:46:57 +0200

    Improve readability and consistency
    Fixed most warnings when inspecting the code

--- alpsh/builtins/exit.py
+++ alpsh/builtins/exit.py
@@ -1,7 +1,8 @@
 import os
-from alpsh.constants import * 
+from alpsh.constants import *
+
 
 def exit(args):
-	os.system('clear')
-	print("Goodbye!")
-	return SHELL_STATUS_STOP
+    os.system('clear')
+    print("Goodbye!")
+    return SHELL_STATUS_STOP
--- alpsh/shell.py
+++ alpsh/shell.py
@@ -1,40 +1,47 @@
-import sys, os, shlex, subprocess
-from constants import *
-from builtins import *
+import shlex
+import subprocess
+import sys
 
-#Hash map to store built-in function name and reference as key and value
+import history as history_listener
+from constants import *
+from alpsh.builtins import *
+
+# Hash map to store built-in function name and reference as key and value
 built_in_cmds = {}
 
+
 def shell_loop():
-    #Start the loop here
+    # Start the loop here
     status = SHELL_STATUS_RUN
 
     while status == SHELL_STATUS_RUN:
-        #Display a command prompt
+        # Display a command prompt
         sys.stdout.write('>')
         sys.stdout.flush()
 
-        #Read the command input
+        # Read the command input
         cmd = sys.stdin.readline()
 
-        #Tokenize the command input
+        # Tokenize the command input
         cmd_tokens = tokenize(cmd)
 
-        #Execute the command and retrieve new status
+        # Execute the command and retrieve new status
         status = execute(cmd_tokens)
+        history_listener.write(cmd)
+
 
 def tokenize(string):
     return shlex.split(string)
 
-def execute(cmd_tokens):
 
+def execute(cmd_tokens):
     cmd_name = cmd_tokens[0]
     cmd_args = cmd_tokens[1:]
-    
+
     if cmd_name in built_in_cmds:
         return built_in_cmds[cmd_name](cmd_args)
 
-    #Execute command
+    # Execute command
     try:
         subprocess.call(cmd_tokens)
     except subprocess.CalledProcessError as error:
@@ -42,19 +49,23 @@ def execute(cmd_tokens):
     except OSError as error:
         print("ERROR 2 : " + str(error))
 
-    #Return status indicating to wait for next command in shell_loop
+    # Return status indicating to wait for next command in shell_loop
     return SHELL_STATUS_RUN
+
 
 def register_command(name, func):
     built_in_cmds[name] = func
-    
+
+
 def init():
     register_command("cd", cd)
     register_command("exit", exit)
-    
+
+
 def main():
     init()
     shell_loop()
+
 
 if __name__ == "__main__":
     main()
