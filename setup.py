abb822cd51da8effc7ca4fe35d76578cfb1562fd
Mozzo <mozzo242@gmail.com>
2017-06-06 09:54:07 +0200

    Fix setup file
    Some lines where not included in the first commit of this file, they
    have now been added.

--- setup.py
+++ setup.py
@@ -4,12 +4,15 @@ import os
 from alpsh import history
 
 
+def create_config():
     location = os.path.expanduser('~') + "/.alpsh"
     print(location)
     if not os.path.exists(location):
         os.makedirs(location)
+        history.create()  # Calls a function inside the actual program to be able to install.
 
 
+create_config()
 
 setup(
     name='alpsh',
