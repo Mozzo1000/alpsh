f8487d1b7607292537aa05f38f437d3908eec5bb
Mozzo <mozzo242@gmail.com>
2017-06-11 15:45:55 +0200

    Add list argument to builtin history
    If you type "history list" as a comand, the content of the
    'alpsh_history.json' file will show in a sorted list.

--- alpsh/builtins/history.py
+++ alpsh/builtins/history.py
@@ -1,6 +1,22 @@
 from alpsh.constants import *
+import os
+import json
 
 
 def history(args):
-    print("hi")
+    print("ARGS : " + str(args))
+    # If no arguments typed
+    if len(args) == 0:
+        print("HOWTO")
+    elif str(args[0]) == "list":
+        # List history in order
+        print("List history")
+        location = os.path.expanduser('~') + "/.alpsh/"
+        with open(location + 'alpsh_history.json', 'r') as history_list:
+            data = json.load(history_list)
+            count = 0
+            for item in range(len(data["history"])):
+                print(count, " : ", data["history"][count])
+                count += 1
+
     return SHELL_STATUS_RUN
