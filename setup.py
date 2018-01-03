207cb48255e304c9f0b05c612b162b3fd32cadde
Mozzo <andreas@simplymozzo.se>
2018-01-03 19:37:41 +0100

    Revert "Added requirements.txt"
    This reverts commit d382ecfc331ae74c1fdc39dba98d219fe35cd313.

--- setup.py
+++ setup.py
@@ -9,11 +9,10 @@ setup(
     author='Mozzo',
     author_email='',
     description='',
-    install_requires=['pyYAML>=3.12'],
+
     entry_points={
         'console_scripts': [
             'alpsh=alpsh.shell:main',
         ],
-
     },
 )
