e3170fabfa70a433fe9090f881153787de49adc2
Mozzo <andreas@simplymozzo.se>
2018-02-04 12:31:54 +0100

    Added centralized variables for the history files location

--- alpsh/constants.py
+++ alpsh/constants.py
@@ -4,6 +4,8 @@ SHELL_STATUS_STOP = 0
 SHELL_STATUS_RUN = 1
 
 LOCATION = os.path.expanduser('~') + "/.alpsh/"
+HISTORY_FILE = 'alpsh_history.json'
+HISTORY_FILE_TMP = 'alpsh_history_tmp'
 
 
 class COLORS:
