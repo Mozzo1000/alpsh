1f909e94eb3a21046046869521cf2aa595fa1895
Mozzo1000 <andreas@simplymozzo.se>
2018-02-12 20:01:48 +0100

    Added working directory to prompt

--- alpsh/prompt.py
+++ alpsh/prompt.py
@@ -1,6 +1,7 @@
 import alpsh.config as config
 import getpass
 import socket
+import os
 
 altered_prompt = ""
 
@@ -12,6 +13,8 @@ def handle_prompt():
         altered_prompt = altered_prompt.replace('@host', socket.gethostname())
     if "@user" in config.get('general', 'prompt'):
         altered_prompt = altered_prompt.replace('@user', getpass.getuser())
+    if "@dir" in config.get('general', 'prompt'):
+        altered_prompt = altered_prompt.replace('@dir', os.path.split(os.getcwd())[1])
 
 
 def get_prompt():
