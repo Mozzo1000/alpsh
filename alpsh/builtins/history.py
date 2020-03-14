76e1f5c3cb288a7ef7b582ba76993eb19e7e6c5e
Andreas Backström <mozzo242@gmail.com>
2020-03-14 19:27:51 +0100

    Change builtin history to display tables
    Closes #14

--- alpsh/builtins/history.py
+++ alpsh/builtins/history.py
@@ -1,4 +1,5 @@
 from alpsh.constants import *
+from rapidtables import make_table
 import os
 import json
 import alpsh.config as config
@@ -16,12 +17,15 @@ def history(args):
         with open(CONFIG_PATH + 'alpsh_history.json', 'r') as history_list:
             data = json.load(history_list)
             count = 0
+            table = []
+            history_data = data['history'][count]
             for item in range(len(data["history"])):
-                print("INDEX : " + str(count))
-                print("COMMAND : " + data['history'][count]['command'])
-                print("TIMESTAMP : " + data['history'][count]['timestamp'])
-                print("SUCCESS : " + data['history'][count]['success'])
+                table.append({'Index': str(count), 'Command': history_data['command'],
+                              "Timestamp": history_data['timestamp'], 'Success': history_data['success']})
                 count += 1
+            tables = make_table(table, tablefmt='md')
+            print(tables)
+            print()
     elif str(args[0] == "clear"):
         print(config.get_setting('text', 'danger')+"-=-=-=-=-=-=-=-WARNING-=-=-=-=-=-=-=\n" +
               COLORS.CLEAR + "You are about to remove your command history.\n" +
