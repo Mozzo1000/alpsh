36ec2d6a26d24b91d5ff87c0c7bde402e4f7e51d
Andreas Backström <mozzo242@gmail.com>
2020-03-15 14:01:44 +0100

    Cleanup builtin ls code

--- alpsh/builtins/ls.py
+++ alpsh/builtins/ls.py
@@ -8,33 +8,30 @@ import alpsh.config as config
 def ls(args):
     # If there is any arguments typed
     if len(args) > 0:
-        if args[0] == "walk":
-            for root, dirs, files in os.walk("."):
-                for filename in files:
-                    print(filename)
-        print("ARGS!")
         if os.path.isdir(args[0]):
-            for link in os.listdir(args[0]):
-                if not link.startswith('.'):
-                    print(str(config.get_setting('general', 'output_color')) + link + COLORS.CLEAR + ' ', end='')
-            print()
+            print_table(args[0])
         elif os.path.isfile(args[0]):
             print("Not a directory!")
     else:
-        table = []
-        for link in os.listdir(os.getcwd()):
-            if not link.startswith('.'):
-                full_path = os.getcwd() + "/" + link
-                size = os.path.getsize(full_path)
-                if os.path.isdir(full_path):
-                    file_type = "Directory"
-                elif os.path.isfile(full_path):
-                    file_type = "File"
-                else:
-                    file_type = "Unknown"
-                table.append({'Name': link, 'Size': utils.convert_byte_size(size), "Type": file_type})
-
-        tables = make_table(table, tablefmt='md')
-        print(tables)
-        print()
+        print_table(os.getcwd())
     return SHELL_STATUS_RUN
+
+
+def print_table(directory):
+    table = []
+    for link in os.listdir(directory):
+        if not link.startswith('.'):
+            full_path = directory + "/" + link
+            print(full_path)
+            size = os.path.getsize(full_path)
+            if os.path.isdir(full_path):
+                file_type = "Directory"
+            elif os.path.isfile(full_path):
+                file_type = "File"
+            else:
+                file_type = "Unknown"
+            table.append({'Name': link, 'Size': utils.convert_byte_size(size), "Type": file_type})
+
+    tables = make_table(table, tablefmt='md')
+    print(tables)
+    print()
