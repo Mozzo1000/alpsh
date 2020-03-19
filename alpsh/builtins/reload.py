263e75ac562fc87742718e7db743ea3480495e2c
Mozzo1000 <andreas@simplymozzo.se>
2020-03-19 13:37:34 +0100

    Change reload builtin to output to log
    Instead of printing out system information to the shell, write a friendly debug message to the log instead.

--- alpsh/builtins/reload.py
+++ alpsh/builtins/reload.py
@@ -1,9 +1,12 @@
 import os
 import sys
+import logging
 from alpsh.constants import *
+
+logger = logging.getLogger(__name__)
 
 
 def reload(args):
-    print(sys.version)
+    logger.debug('Reloading shell')
     os.execv(sys.executable, ['python'] + sys.argv)
     return SHELL_STATUS_STOP
